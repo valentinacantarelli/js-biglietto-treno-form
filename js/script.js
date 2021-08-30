97bcda7c17aa6049d2b59b07c506a4863c68d4e9
valentina cantarelli <valentinacanterina@gmail.com>
2021-08-30 15:48:02 +0200

    create var form

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
-// Il programma dovrà mostrare una form da compilare con cui chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+//Nome, il numero km da percorrere e l'età del passeggero.
+// calcolare il prezzo totale del viaggio, secondo queste regole:
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
-// Il recap dei dati e l'output del prezzo finale va stampato in pagina (formattato con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+// Il recap dei dati e l'output del prezzo finale va stampato in pagina 
+
+var nome=document.getElementById("nome").value;
+var distanza=parseInt(document.getElementById("distanza").value);
+var fasciaEta=document.getElementById("fascia-eta").value;
